a001da06628f622ff3a0f7a86883009ef4de9c25
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-02-26 13:50:23 -0800

    gosh

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
-"""First endpoint route to return API status"""
-from flask import Flask
-from os import environ
+"""
+Flask App that integrates AirBnB static HTML Template
+"""
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+import os
+
+# Set flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
 
 app = Flask(__name__)
-
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
+# Create a CORS instance
+cors = CORS(app, resources={r"/*": {"origins": '0.0.0.0'}})
+
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Close storage"""
+def teardown(exception):
+    """Close the storage session after each request."""
     storage.close()
 
-if __name__ == "__main__":
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
 
+@app.errorhandler(404)
+def handle_api_error(exception):
+    """Returns a JSON-formatted 404 status code response."""
+    return make_response(jsonify({
+                    "error": "Not found"
+                    }), 404)
+
+
+if __name__ == "__main__":
+    # start Flask app
+    app.run(host=host, port=port, threaded=True)
