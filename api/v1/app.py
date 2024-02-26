990f8e3625520f18e9fd12c76d457a392c560a4e
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-02-26 07:46:04 -0800

    cool

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,8 +2,8 @@
 """firts endpoint route to return api status"""
 from flask import flask
 from os import environ
-import storage from models
-import app_views from app.v1.views
+from models import storage
+from app.v1.views import app_views
 
 app = flask (__name__)
 
@@ -14,7 +14,7 @@ app.register_blueprint()app_views)
         """ close storage """
         storage.close()
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
